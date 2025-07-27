70fd444268d302a31ecf605fdc5ab26d618b8ff1
Flicko75 <142166063+Flicko75@users.noreply.github.com>
2025-07-27 20:56:28 +0530

    delete products from html

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 3
 }, {
@@ -25,7 +25,7 @@ export function addToCart(productId) {
 }
 
 export function removeFromCart(productId) {
-    const newCart = [];
+    let newCart = [];
 
     cart.forEach((cartItem) => {
         if (cartItem.productId !== productId){
