9dce594350c2c9aa205c52dbd926b9c7de8d4e7e
Flicko75 <142166063+Flicko75@users.noreply.github.com>
2025-07-31 21:57:22 +0530

    convert xhr to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,15 +2,11 @@ import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 // import '../data/cart-class.js';
-import { loadProducts } from '../data/products.js';
+import { loadProducts, loadProductsFetch } from '../data/products.js';
 import { loadCart } from '../data/cart.js';
 
 Promise.all([
-    new Promise((resolve) => {
-        loadProducts(() => {
-            resolve('value 1');
-        });
-    }),
+    loadProductsFetch(),
     new Promise((resolve) => {
         loadCart(() => {
             resolve();
