e4ae710e39e4bfa17a9d83699f9eea247abd16f4
Flicko75 <142166063+Flicko75@users.noreply.github.com>
2025-07-31 21:17:13 +0530

    load products from backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,10 @@ import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 // import '../data/cart-class.js';
+import { loadProducts } from '../data/products.js';
 
-renderOrderSummary();
-renderPaymentSummary();
-renderCheckoutHeader();
+loadProducts(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+    renderCheckoutHeader();
+});
